1fbb1da9247d06d589a51e8e066cb5400c3992ca
Baxter Ross <baxterross@gmail.com>
2013-07-28 13:07:18 -0700

    adding initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Programnotes::Application.config.secret_key_base = '9a604e321ca514281e64b9b7fc16829b7b6353a2f5fc23a13d6bbb3bd56e08a9e7bc128807a7feb6a229c69015807ecffc45c130dafb4138a0ef31eadde3eb6f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Programnotes::Application.config.session_store :cookie_store, key: '_programnotes_session'
